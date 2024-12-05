153e3985f733477beb5543dbd678432db616d60e
jadheer <30946328+jadheer@users.noreply.github.com>
2024-12-05 11:47:58 +0530

    Delete emp_enum.sql

--- emp_enum.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE employee (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    phone_number VARCHAR(15),
-    position ENUM('Manager', 'Developer', 'Designer', 'Tester', 'HR') NOT NULL
-);
